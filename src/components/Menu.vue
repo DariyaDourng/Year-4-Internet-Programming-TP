a0b8cc2a3995135015d5678c274675dc5226fd46
DariyaDourng <126034461+DariyaDourng@users.noreply.github.com>
2023-11-18 14:50:05 +0700

    Src folder update

---
+++ src/components/Menu.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        name: "Menu",
+        props: {
+            title: String,
+            element: String,
+            menus: []
+        }
+    }
+</script>
+<template>
+    <div class="flex items-center justify-between w-full">
+        <h1 class="font-bold text-2xl">{{ title }}</h1>
+        <div class="flex gap-4">
+            <span class="font-bold">All</span>
+            <p v-for="menu in menus" class="text-slate-700">{{ menu }}</p>
+        </div>
+    </div>
+</template>
